adf806575e6ca6ef63194fac76bd2f2064fe5b7f
galvin <teyasio@comcast.net>
2016-07-25 03:54:45 +0000

    Possible fix for elvui

--- DefaultUB.lua
+++ DefaultUB.lua
@@ -10,7 +10,7 @@
 local MyAddon, GUB = ...
 
 GUB.DefaultUB = {}
-GUB.DefaultUB.Version = 524
+GUB.DefaultUB.Version = 525
 
 -------------------------------------------------------------------------------
 -- UnitBar table data structure.
