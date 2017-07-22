335ebad7abc2a40286b4f944f2f610cf49ad9cef
galvin <galvin@08cb8494-e0d1-4228-a1df-7392276cee23>
2017-07-22 08:06:30 +0000

    Added a check for missing lua code from the TOC file.

--- trunk/DefaultUB.lua
+++ trunk/DefaultUB.lua
@@ -10,7 +10,7 @@
 local MyAddon, GUB = ...
 
 GUB.DefaultUB = {}
-GUB.DefaultUB.Version = 550
+GUB.DefaultUB.Version = 551
 
 -------------------------------------------------------------------------------
 -- UnitBar table data structure.
