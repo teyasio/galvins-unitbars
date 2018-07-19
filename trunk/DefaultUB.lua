ecd2ca9d58338e96f2439382f54ce36fcc8c25e0
galvin <galvin@08cb8494-e0d1-4228-a1df-7392276cee23>
2018-07-19 17:11:27 +0000

    Fixed a bug where GetPvpTalentSlotInfo can return nil

--- trunk/DefaultUB.lua
+++ trunk/DefaultUB.lua
@@ -10,7 +10,7 @@
 local MyAddon, GUB = ...
 
 GUB.DefaultUB = {}
-GUB.DefaultUB.Version = 601
+GUB.DefaultUB.Version = 602
 
 -------------------------------------------------------------------------------
 -- UnitBar table data structure.
