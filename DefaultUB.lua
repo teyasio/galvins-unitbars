610d8fbde4449fc44ae77bbebcfab416cef3dab4
galvin <teyasio@comcast.net>
2018-10-28 03:47:50 +0000

    Fixed a coding error in main.lua for returning white when reaction color returns nil

--- DefaultUB.lua
+++ DefaultUB.lua
@@ -10,7 +10,7 @@
 local MyAddon, GUB = ...
 
 GUB.DefaultUB = {}
-GUB.DefaultUB.Version = 605
+GUB.DefaultUB.Version = 606
 
 -------------------------------------------------------------------------------
 -- UnitBar table data structure.
